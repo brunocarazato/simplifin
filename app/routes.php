27dd3e43f5c8c6b6b5be8295df19f6f5773557a2
Bruno Carazato <brunocarazatoo@gmail.com>
2025-04-06 09:43:26 -0300

    feat: Adicionar rota transfer - closes #11

--- app/routes.php
+++ app/routes.php
@@ -4,8 +4,16 @@ declare(strict_types=1);
 
 use App\Application\Actions\User\ListUsersAction;
 use App\Application\Actions\User\ViewUserAction;
+use App\Application\Actions\Transfer\TransferAction;
+use App\Infrastructure\Models\User;
+use App\Domain\MerchantUser\MerchantUser;
+use App\Domain\CommonUser\CommonUser;
+use App\Application\Services\Transfer\TransferService;
+use App\Application\Services\Transfer\CurlClient;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
+use App\Application\Services\Transfer\AuthorizationService;
+use App\Application\Services\Transfer\NotificationService;
 use Slim\App;
 use Slim\Interfaces\RouteCollectorProxyInterface as Group;
 
@@ -16,12 +24,11 @@ return function (App $app) {
     });
 
     $app->get('/', function (Request $request, Response $response) {
-        $response->getBody()->write('Hello world!');
+        $response->getBody()->write("Bem vindo(a) !");
+
         return $response;
     });
 
-    $app->group('/users', function (Group $group) {
-        $group->get('', ListUsersAction::class);
-        $group->get('/{id}', ViewUserAction::class);
-    });
+    $app->post('/transfer', TransferAction::class);
+
 };
