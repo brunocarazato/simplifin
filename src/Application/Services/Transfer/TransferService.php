6c5d980f9189d00c072a8124dc2cf2462d12034a
Bruno Carazato <brunocarazatoo@gmail.com>
2025-04-06 09:46:24 -0300

    feat: Adicionar transaction e persistir - closes #15

--- src/Application/Services/Transfer/TransferService.php
+++ src/Application/Services/Transfer/TransferService.php
@@ -6,26 +6,34 @@ namespace App\Application\Services\Transfer;
 
 use App\Domain\User\User;
 use App\Application\Services\Transfer\Exception\ForbiddenTransferException;
+use Illuminate\Database\Capsule\Manager as DB;
+use App\Domain\User\UserRepositoryInterface;
 
 class TransferService
 {
     public function __construct(
         private AuthorizationService $authorizationService,
-        private NotificationService $notificationService
+        private NotificationService $notificationService,
+        private UserRepositoryInterface $userRepository
     ) {}
 
     public function transfer(User $payer, User $payee, float $amount): void
     {
 
-        $payer->canTransfer();
-
-        if (!$this->authorizationService->isAuthorized()) {
-            throw new ForbiddenTransferException();
-        }
-
-        $payer->debit($amount);
-        $payee->credit($amount);
-
-        $this->notificationService->notify($payee);
+        DB::transaction(function () use ($payer, $payee, $amount) {
+            $payer->canTransfer();
+    
+            if (!$this->authorizationService->isAuthorized()) {
+                throw new ForbiddenTransferException();
+            }
+    
+            $payer->debit($amount); 
+            $payee->credit($amount);
+    
+            $this->userRepository->save($payer);
+            $this->userRepository->save($payee);
+    
+            $this->notificationService->notify($payee);
+        });
     }
 }
